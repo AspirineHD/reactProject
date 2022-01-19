36001ea1949e2e7eaf2db14253a0d80080d85b59
Tawan Wareeboot <47499678+AspirineHD@users.noreply.github.com>
2022-01-19 10:54:12 +0700

    New reactProject
    New reactProject

---
+++ screens/HomeScreen.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+
+// import Ionicons from 'react-native-vector-icons/Ionicons'
+
+const HomeScreen = ({navigation}) => {
+    return (
+        <View style={styles.container}>
+            {/* <Ionicons name='home' size={30} color='skyblue'/> */}
+            <Text>หน้าหลัง</Text>
+            <Button
+                title="Go to About"
+                onPress={() => navigation.navigate('About',{
+                    email:'MiyukiS@gmail.com'
+                }
+                )}
+            />
+        </View>
+    )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+    }
+})
